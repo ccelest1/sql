d4c392c60908f5be5fd8940a409bd2b6c266356a
Craig Tyler Celestin <crgcelestin@gmail.com>
2024-08-07 22:17:15 -0400

    sql edits

--- data-lemur/duplicate-jobs.sql
+++ data-lemur/duplicate-jobs.sql
@@ -5,7 +5,7 @@
   g -> duplicate_companies
   h -> count(count_id) >=2
 */
-with job_count as (
+with job_counts as (
   select company_id, title, description,
   count(job_id) as job_count
   from job_listings
@@ -13,5 +13,5 @@ with job_count as (
 )
 
 select count(distinct company_id) as duplicate_companies
-from job_count
+from job_counts
 where job_count>1;
--- data-lemur/postHiatus-facebook.sql
+++ data-lemur/postHiatus-facebook.sql
@@ -23,7 +23,7 @@ select user_id,
       max(post_date::date) - min(post_date::date)
       as days_between
 from posts
-WHERE date_part('year',post_date::date)=2021
+WHERE date_part('year',post_date)=2021
 group by user_id
 having count(post_id)>1;
 
